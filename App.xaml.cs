d00d1653c5478fb2887e58bbcdc10f386ddda7cc
Fotters05 <t50_n.n.kudrov@mpt.ru>
2024-06-06 13:39:15 +0300

    pr8

---
+++ App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows;
+
+namespace Biblioteka
+{
+    public partial class App : Application
+    {
+        private static string theme;
+        public static string Theme
+        {
+            get { return theme; }
+            set
+            {
+                theme = value;
+                var dict = new ResourceDictionary { Source = new Uri("/Theme;component/themes/" + value + ".xaml", UriKind.Relative) };
+
+                Current.Resources.MergedDictionaries.RemoveAt(0);
+                Current.Resources.MergedDictionaries.Insert(0, dict);
+
+                var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                File.WriteAllText($"{desktop}\\theme.txt", value);
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (File.Exists($"{desktop}\\theme.txt"))
+            {
+                Theme = File.ReadAllText($"{desktop}\\theme.txt");
+            }
+        }
+    }
+}
